334d2b0afce78037e1e467b44bf44792fe781c9e
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-01-07 17:33:14 -0300

    estudando if e else em javascript

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+let vel = 60
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {//condição simples
+    console.log(`Você ultrapassou o limite de velocidade`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
