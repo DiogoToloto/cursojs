374c4cf76cfc481053e3ed997db9d987226130ba
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-01-09 05:08:32 -0300

    estudando if, else, else if

---
+++ aula012/ex012.js
@@ -0,0 +1,10 @@
+let agora = new Date()
+let hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora >= 0 && hora <= 12) {
+    console.log('Bom dia')
+} else if (hora >= 13 && hora <= 18) {
+    console.log('Boa tarde')
+} else {
+    console.log('Boa noie')
+}
