b3c8d3323e0e8ab09dbdbdad1ee80fdf5f4f1c1b
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-01-21 18:49:07 -0300

    projeto tabuada

--- aula015ex/ex017/script.js
+++ aula015ex/ex017/script.js
@@ -1,19 +1,20 @@
 function tabuada() {
-    let num = document.getElementById('inumero')
-    let tab = document.getElementById('seltab')
 
-    if (num.value.length == 0) {
-        window.alert('Por favor digite um numero')
-    }
- else {
+let num = document.getElementById('inumero')
+let tab = document.getElementById('seltab')
+
+if (num.value.length == 0) {
+    window.alert('[ERRO] insira algum dado')
+} else {
     let n = Number(num.value)
     let c = 1
     tab.innerHTML = ''
+
     while (c <= 10) {
         let item = document.createElement('option')
         item.text = `${n} x ${c} = ${n*c}`
         tab.append(item)
         c++
+        }
     }
- }
 }
