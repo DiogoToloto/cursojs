f28b8a22f18d61070d17ca143dd13483a75487ad
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-01-28 13:16:36 -0300

    exercicio adicionando valores da array

---
+++ aula018ex/desafio01/app.js
@@ -0,0 +1,36 @@
+let numero = document.getElementById('inumero')
+let selecao = document.getElementById('selecao')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(numero.value) && !inLista(numero.value, valores)) {
+        valores.push(Number(numero.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${numero.value} adicionado`
+        selecao.append(item)
+
+    } else {
+        window.alert('Valor invalido ou já encontrado na lista.')
+    }
+
+    numero.value = ''
+    numero.focus()
+}
